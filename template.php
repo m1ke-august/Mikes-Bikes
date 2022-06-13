63bea9d7908d9935c1150b84fd161ea8c5b70be9
m1ke-august <98857498+m1ke-august@users.noreply.github.com>
2022-06-14 11:59:10 +1200

    template code

---
+++ template.php
@@ -0,0 +1,21 @@
+<!-- Template for Page Layout-->
+
+<?php include('');
+	$page_title=" enter title here "
+?>
+
+<div class="container pt-3 w3-theme-l1">
+	<div class="row">
+        <div class="col-sm-12">
+        
+        <h2 class="text-center h2">heading text here</h2>
+        
+        <hr />
+
+		</div>
+
+	</div>
+</div>
+
+<?php include('');
+?>
