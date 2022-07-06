7e73ecfad32c790ef380acd6d017b1d45cd25413
m1ke-august <98857498+m1ke-august@users.noreply.github.com>
2022-07-06 13:21:58 +1200

    commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- Home Page -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Navbar</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+  </head>
+<?php include('includes/navbar.php');
+	$page_title=" Home Page "
+?>
+
+<div class="container pt-3 w3-theme-l1">
+	<div class="row">
+        <div class="col-sm-12">
+        
+        <h2 class="text-center h2"></h2>
+        
+        <hr />
+
+		</div>
+
+	</div>
+</div>
+
+</html>
--- template.php
+++ template.php
@@ -19,3 +19,49 @@
 
 <?php include('');
 ?>
+
+<!-- Header -->
+
+<!doctype html>
+<html lang=en> 
+<head>
+
+<!--Required Meta Tags-->
+<title>
+	
+<?php echo $page_title;
+?>
+</title>
+<meta name="keywords" content="ford, cars">
+<meta name = "descriptions" content=" will ensure that you can find a Ford vehicle that suits you best!">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+
+<!--Link To Bootstrap CSS-->
+<script src="SpryAssets/SpryValidationTextField.js" ></script>
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css">
+
+<script src="SpryAssets/SpryValidationTextarea.js" ></script>
+<link href="SpryAssets/SpryValidationTextarea.css" rel="stylesheet" type="text/css" />
+
+
+<!--Link To Bootstrap CSS-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<!--Link To Bootstrap JS File-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!--- Link to Awesome fonts - External Link -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+<!--Google Fonts Links-->
+<link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Yomogi&display=swap" rel="stylesheet">
+
+<!--Link To Favicon-->
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+
+
+</head>
