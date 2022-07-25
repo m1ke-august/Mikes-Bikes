6829c37d3bf1acfa01346d76e5c3b7987d20749e
m1ke-august <98857498+m1ke-august@users.noreply.github.com>
2022-07-26 11:50:33 +1200

    commit

--- template.php
+++ template.php
@@ -45,10 +45,8 @@
 <link href="SpryAssets/SpryValidationTextarea.css" rel="stylesheet" type="text/css" />
 
 
-<!--Link To Bootstrap CSS-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-<!--Link To Bootstrap JS File-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <!--- Link to Awesome fonts - External Link -->
