9679f84f6ffc3b455dd0d45393575d708e6b9454
m1ke-august <98857498+m1ke-august@users.noreply.github.com>
2022-08-01 13:33:33 +1200

    commit

--- navtemplate.php
+++ navtemplate.php
@@ -1,27 +1,3 @@
-<?php
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $mobileNumber = $_POST['mobileNumber'];
-    $password = $_POST['password'];
-
-
-    //Database Connection
-    $conn = new mysqli('localhost','root','','mikesbikes');
-    if($conn->connect_error){
-        die('Connection Error : ' .$conn->connect_error);
-    }else{
-        $stmt = $conn->prepare("insert into signup(firstName, lastName, email, mobileNumber, password) 
-            values(?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssis", $firstName, $lastName, $email, $mobileNumber, $password);
-        $stmt->execute();
-        echo "Successfully Signed Up";
-        $stmt->close();
-        $conn->close();
-    }
-?>
-
-<!--Code Successfuly Signed Up Page here-->
 <!DOCTYPE html>
 <html>
     <head>
